b8245853d0cb08b19d927605a2e5b303953cfa28
Zafiirah Hosenie <A2Z@XiaoMiRepeater.home>
2021-02-01 15:47:35 +0000

    Create util.py

---
+++ meerCRAB_code/util.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python
+"""
+Authors : Zafiirah Hosenie
+Email : zafiirah.hosenie@gmail.com or zafiirah.hosenie@postgrad.manchester.ac.uk
+Affiliation : The University of Manchester, UK.
+License : MIT
+Status : Under Development
+Description :
+Python implementation for Classification of Bogus and Real using Deep Learning for MeerLICHT facility.
+(paper can be found here : https://arxiv.org/abs/---).
+This code is tested in Python 3 version 3.5.3  
+"""
+
+import os
+import sys
+import json
+
+
+def makedirs(d):
+    '''Make a directory if it does not exist'''
+    if not os.path.exists(d):
+        os.makedirs(d)
+            
+def ensure_dir(f):
+    '''Ensure that the directory exist, if it does not exist, create one automatically'''
+    
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        try:
+            os.makedirs(d)
+        except:
+            pass
